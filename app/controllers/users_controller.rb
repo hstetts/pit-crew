108ac5f3bded6b5b80f20b4a47a36f0336d3b2bf
hstetts@gmail.com <hstetts@gmail.com>
2018-05-15 23:42:57 -0400

    Contact page and added hero image to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+
 
   end
 
   def create
-    @user = User.new(params[:user])
-
-    @user.first_name = params[:user][:first_name]
-    @user.last_name = params[:user][:last_name]
-    @user.phone_number = params[:user][:phone_number]
-    @user.phone_type = params[:user][:phone_type]
-    @user.address = params[:user][:address]
-    @user.city = params[:user][:city]
-    @user.state = params[:user][:state]
-    @user.zip_code = params[:user][:zip_code]
-    @user.address_type= params[:user][:address_type]
-    @user.preferred_contact = params[:user][:preferred_contact]
-    @user.marketing_opt = params[:user][:marketing_opt]
-    @user.email = params[:user][:email]
-    @user.existing_customer = params[:user][:existing_customer]
+    # @user = User.new(params[:user])
+    #
+    # @user.first_name = params[:user][:first_name]
+    # @user.last_name = params[:user][:last_name]
+    # @user.phone_number = params[:user][:phone_number]
+    # @user.phone_type = params[:user][:phone_type]
+    # @user.address = params[:user][:address]
+    # @user.city = params[:user][:city]
+    # @user.state = params[:user][:state]
+    # @user.zip_code = params[:user][:zip_code]
+    # @user.address_type= params[:user][:address_type]
+    # @user.preferred_contact = params[:user][:preferred_contact]
+    # @user.marketing_opt = params[:user][:marketing_opt]
+    # @user.email = params[:user][:email]
+    # @user.existing_customer = params[:user][:existing_customer]
 
     # if @user.save
     #   flash[:success] = "User Added to Database!"
